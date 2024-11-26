b5459c96d295a4021ac2134c7e1a69156fec19ce
abirmes <abirmeskini02@gmail.com>
2024-11-26 14:25:43 +0100

    almost finishing positionnement

--- script.js
+++ script.js
@@ -2,19 +2,42 @@ async function CallAPI() {
     let url = "players.json";
     let fetcher = await fetch(url);
     let json = await fetcher.json();
-    
+
     console.log(json);
-    
+    bringCards(json);
+
 }
 
 CallAPI();
 let container = document.getElementById("container")
-function bringCards(players){
-    container.innerHTML="";
+function bringCards(players) {
+    container.innerHTML = "";
     players.forEach(player => {
-        container.innerHTML+=`
-        <div class="col-md-6">
+        container.innerHTML += `
+        <div class="col-md-6" style="background-image: url(src/assets/img/placeholder-removebg.png);">
+            <div class="row">
+                <div class="col-md-10">
+                    <img src="${player.photo}" alt="playerphoto"/>
+                </div>
+                <div class="col-md-2">
 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <p><strong>PAC</strong></br>${player.pace}</p>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
         <div>
         `
     });
